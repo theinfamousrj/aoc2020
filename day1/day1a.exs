af3d9ed6651b3dedbd926ce19b092f5817a6f89a
RJ Hill <infamous.rj@gmail.com>
2020-12-02 16:03:49 -0500

    Create day1a.exs

---
+++ day1/day1a.exs
@@ -0,0 +1,23 @@
+{:ok, body} = File.read("input1a")
+
+lines = String.split(body, "\n", trim: true)
+
+lines
+|> Enum.with_index
+|> Enum.each(fn({x,i}) -> 
+  lines
+  |> Enum.with_index
+  |> Enum.each(fn({y,j}) -> 
+    {intX, blahX} = Integer.parse(x)
+    {intY, blahY} = Integer.parse(y)
+    
+    sum = intX + intY
+    mul = intX * intY
+    
+    if intX + intY == 2020 do
+      IO.puts("#{i} & #{j}")
+      IO.puts("#{x} + #{y} = #{sum}")
+      IO.puts("#{x} * #{y} = #{mul}")
+    end
+  end)
+end)
